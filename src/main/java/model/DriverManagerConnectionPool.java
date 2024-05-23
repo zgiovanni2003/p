6611d1402146a321db5ce37fccdba975ba297672
zgiovanni2003 <zgiovanni2003@gmail.com>
2024-05-23 14:16:18 +0200

    id-1 resolved

--- src/main/java/model/DriverManagerConnectionPool.java
+++ src/main/java/model/DriverManagerConnectionPool.java
@@ -24,7 +24,7 @@ public class DriverManagerConnectionPool {
 		Connection newConnection = null;
 		String db = "GeekFactoryDB";
 		String username = "root";
-		String password = "root";
+		String password = "";
 
 		newConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/"+db, username, password);
 		
--- src/main/java/model/ProductModel.java
+++ src/main/java/model/ProductModel.java
@@ -121,13 +121,14 @@ public class ProductModel {
 
 		Collection<ProductBean> products = new LinkedList<ProductBean>();
 
-		String selectSQL = "SELECT * FROM " + ProductModel.TABLE_NAME + " WHERE deleted = 'false' AND nomeTipologia = '" + where + "'";
+		String selectSQL = "SELECT * FROM " + ProductModel.TABLE_NAME + " WHERE deleted = 'false' AND nomeTipologia = ? ";
 		String sql2 = "SELECT AVG(votazione) FROM Recensione WHERE codiceProdotto = ?";
 		
 		try {
 			connection = DriverManagerConnectionPool.getConnection();
+			
 			preparedStatement = connection.prepareStatement(selectSQL);
-
+			preparedStatement.setString(1, where);
 			ResultSet rs = preparedStatement.executeQuery();
 
 			while (rs.next()) {
